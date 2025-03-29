1fbccbc7bdaf864aa5e91bffc4f79f31fd3844ec
186500-hvl <eirikinnselset@hotmail.com>
2025-03-29 17:02:55 +0100

    awooakwdakowd

--- src/Dao/AvdelingDao.java
+++ src/Dao/AvdelingDao.java
@@ -1,5 +1,24 @@
 package Dao;
 
-public class AvdelingDao {
+import model.Avdeling;
 
+import javax.persistence.*;
+
+public class AvdelingDAO {
+    private EntityManager em;
+
+    public AvdelingDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public Avdeling finnAvdelingMedId(int id) {
+        return em.find(Avdeling.class, id);
+    }
+
+    public void lagreNyAvdeling(Avdeling avdeling) {
+        em.getTransaction().begin();
+        em.persist(avdeling);
+        em.getTransaction().commit();
+    }
 }
+
